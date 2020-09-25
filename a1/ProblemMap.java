c356a99f77bc118635ba38688fa1b846d8a825f1
danieldubichev <dannydubichev@gmail.com>
2020-09-24 19:47:13 -0700

    Q4 done; need Q5 and Q6 to finish still

---
+++ a1/ProblemMap.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ProblemMap extends Problem {
+	Map<String, Map<String, Double>> map;
+	Map<String, Double> sld;
+	
+	public Object goalState;
+	
+	public ProblemMap(String mapfilename) throws Exception {
+		map = new HashMap<String, Map<String, Double>>();
+		//read map from file of source-destination-cost triples (tab separated)
+    	BufferedReader reader = new BufferedReader( new FileReader (mapfilename));
+        String line;
+        while( ( line = reader.readLine() ) != null ) {
+        	String[] strA = line.split("\t");
+        	
+        	String 	from_city = strA[0], 
+        			to_city   = strA[1];
+        	Double 	cost 	  = Double.parseDouble(strA[2]);
+        	
+        	if(!map.containsKey(from_city)) 
+        		map.put(from_city, new HashMap<String, Double>());
+        	map.get(from_city).put(to_city,cost);
+        	
+        	//putting the reverse edge as well
+        	if(!map.containsKey(to_city)) 
+        		map.put(to_city, new HashMap<String, Double>());
+        	map.get(to_city).put(from_city,cost);
+        }
+        reader.close();
+	}
+	
+	public ProblemMap(String mapfilename, String heuristicfilename) throws Exception {
+		this(mapfilename);
+		
+		sld = new HashMap<String, Double>();
+    	BufferedReader reader = new BufferedReader( new FileReader (heuristicfilename));
+        String line;
+        while( ( line = reader.readLine() ) != null ) {
+        	String[] strA = line.split("\t");
+        
+        	String 	city = strA[0]; 
+        	double 	h 	 = Double.parseDouble(strA[1]);
+        	
+        	sld.put(city, h);
+        }
+        reader.close();
+	}
+	
+	boolean goal_test(Object state) {
+		return state.equals(goalState);
+	}
+
+	Set<Object> getSuccessors(Object state) {
+		Set<Object> result = new HashSet<Object>();
+		for(Object successor_state : map.get(state).keySet())
+			result.add(successor_state);
+		return result;
+	}
+	
+	double step_cost(Object fromState, Object toState) {
+		return map.get(fromState).get(toState);
+	}
+
+	public double h(Object state) {
+		return sld.get(state);
+	}
+
+	public static void main(String[] args) throws Exception {
+		ProblemMap problem = new ProblemMap("romania.txt","romaniaSLD.txt");
+		problem.initialState = "Timisoara";
+		problem.goalState = "Bucharest";
+		
+		Search search  = new Search(problem);
+		
+		System.out.println("TreeSearch------------------------");
+		/*
+		System.out.println("BreadthFirstTreeSearch:\t\t" + search.BreadthFirstTreeSearch());
+		System.out.println("UniformCostTreeSearch:\t\t" + search.UniformCostTreeSearch());
+		System.out.println("DepthFirstTreeSearch:\t\t" + search.DepthFirstTreeSearch());
+		System.out.println("GreedyBestFirstTreeSearch:\t" + search.GreedyBestFirstTreeSearch());
+		System.out.println("AstarTreeSearch:\t\t" + search.AstarTreeSearch());
+		*/
+
+		//for question 4 part 1A = complete
+		//note: prints the execution in order, then prints the solution route, can comment out if wanting just solution
+		System.out.println("AstarTreeSearch:\t\t" + search.AstarTreeSearch2());
+		
+		
+		//System.out.println("\n\nGraphSearch----------------------");
+		/*
+		System.out.println("BreadthFirstGraphSearch:\t" + search.BreadthFirstGraphSearch());
+		System.out.println("UniformCostGraphSearch:\t\t" + search.UniformCostGraphSearch());
+		System.out.println("DepthFirstGraphSearch:\t\t" + search.DepthFirstGraphSearch());
+		System.out.println("GreedyBestGraphSearch:\t\t" + search.GreedyBestFirstGraphSearch());
+		System.out.println("AstarGraphSearch:\t\t" + search.AstarGraphSearch());
+		*/
+		
+		//for question 4 part 2 = complete.
+		//note: prints the execution in order, then prints the solution route, can comment out if wanting just solution
+		System.out.println("AstarGraphSearch:\t\t" + search.AstarGraphSearch2());
+		
+		/*
+		System.out.println("\n\nIterativeDeepening----------------------");
+		System.out.println("IterativeDeepeningTreeSearch:\t" + search.IterativeDeepeningTreeSearch());
+		System.out.println("IterativeDeepeningGraphSearch:\t" + search.IterativeDeepeningGraphSearch());
+		*/
+	}
+	
+}
---
+++ a1/Search.java
@@ -0,0 +1,310 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Search {
+	Problem problem;
+	
+	public Search(Problem problem) { this.problem = problem; }
+	
+	//Tree-search methods
+	public String BreadthFirstTreeSearch() {
+		return TreeSearch(new FrontierFIFO());
+	}
+	
+	public String DepthFirstTreeSearch() {
+		return TreeSearch(new FrontierLIFO());
+	}
+	
+	public String UniformCostTreeSearch() {
+		return TreeSearch(new FrontierPriorityQueue(new ComparatorG()));
+	}	
+
+	public String GreedyBestFirstTreeSearch() {
+		return TreeSearch(new FrontierPriorityQueue(new ComparatorH(problem)));
+	}
+	
+	public String AstarTreeSearch() {
+		return TreeSearch(new FrontierPriorityQueue(new ComparatorF(problem)));
+	}
+	public String AstarTreeSearch2() {
+		return TreeSearch2(new FrontierPriorityQueue(new ComparatorF(problem)));
+	}
+	
+	//Graph-search methods
+	public String BreadthFirstGraphSearch() {
+		return GraphSearch(new FrontierFIFO());
+	}
+	
+	public String DepthFirstGraphSearch() {
+		return GraphSearch(new FrontierLIFO());
+	}
+	
+	public String UniformCostGraphSearch() {
+		return GraphSearch(new FrontierPriorityQueue(new ComparatorG()));
+	}	
+
+	public String GreedyBestFirstGraphSearch() {
+		return GraphSearch(new FrontierPriorityQueue(new ComparatorH(problem)));
+	}
+	
+	public String AstarGraphSearch() {
+		return GraphSearch(new FrontierPriorityQueue(new ComparatorF(problem)));
+	}
+	public String AstarGraphSearch2() {
+		return GraphSearch2(new FrontierPriorityQueue(new ComparatorF(problem)));
+	}
+
+	
+	//Iterative deepening, tree-search and graph-search
+	public String IterativeDeepeningTreeSearch() {
+		//TODO
+		return null;
+	}
+	
+	public String IterativeDeepeningGraphSearch() {
+		//TODO
+		return null;	
+	}
+	
+	//For statistics purposes
+	int cnt; //count expansions
+	List<Node> node_list; //store all nodes ever generated
+	Node initialNode; //initial node based on initial state
+	//
+	private String TreeSearch2(Frontier frontier) {
+		cnt = 0; 
+		node_list = new ArrayList<Node>();
+		
+		initialNode = MakeNode(problem.initialState); 
+
+		//queue to store info about traversal
+		Queue<String> visited = new ArrayDeque<String>();
+
+		node_list.add( initialNode );
+		
+		frontier.insert( initialNode );
+		while(true) {
+			
+			if(frontier.isEmpty()){
+				return null;
+			}
+			Node node = frontier.remove();
+			
+			//get information about node
+			String cur = ( node.state + "(g=" + node.path_cost  + " , h=" + problem.h(node.state)+ ", f=" 
+			+ ((double)problem.h(node.state) + (double)node.path_cost) + ") order=" + (cnt));
+
+			//save node info into queue
+			visited.add(cur);		
+			
+			if( problem.goal_test(node.state) ){
+				//quick function to make the order updated to reflect bucharest; so it does not return -1;
+				node.order = cnt;
+				//print all visited route of the tree
+				System.out.println("Entire traversal of tree in order: Execution of TreeSearch");
+				PrintTreeAllVisited(visited);
+				//print the tree valid route
+				System.out.println("Traversal of solution route: Using TreeSearch");
+				System.out.println(PrintTree(node));
+				return Solution(node);
+			}
+			frontier.insertAll(Expand(node,problem));
+			cnt++;
+		}
+	}
+	private String TreeSearch(Frontier frontier) {
+		cnt = 0; 
+		node_list = new ArrayList<Node>();
+		
+		initialNode = MakeNode(problem.initialState); 
+		node_list.add( initialNode );
+		
+		frontier.insert( initialNode );
+		while(true) {
+			
+			if(frontier.isEmpty())
+				return null;
+			
+			Node node = frontier.remove();
+			
+			if( problem.goal_test(node.state) )
+				return Solution(node);
+			
+			frontier.insertAll(Expand(node,problem));
+			cnt++;
+		}
+	}
+
+	private String GraphSearch(Frontier frontier) {
+		cnt = 0; 
+		node_list = new ArrayList<Node>();
+		
+		initialNode = MakeNode(problem.initialState); 
+		node_list.add( initialNode );
+		
+		Set<Object> explored = new HashSet<Object>(); //empty set
+		frontier.insert( initialNode );
+		while(true) {
+			
+			if(frontier.isEmpty())
+				return null;
+			
+			Node node = frontier.remove();
+			
+			if( problem.goal_test(node.state) ){
+				return Solution(node);
+			}
+			
+			if( !explored.contains(node.state) ) {
+				explored.add(node.state);
+				frontier.insertAll(Expand(node,problem));
+				cnt++;
+			}
+		}
+	}
+	private String GraphSearch2(Frontier frontier) {
+		cnt = 0; 
+		node_list = new ArrayList<Node>();
+		
+		//queue to store info about traversal
+		Queue<String> visited = new ArrayDeque<String>();
+
+		initialNode = MakeNode(problem.initialState); 
+		node_list.add( initialNode );
+		
+		Set<Object> explored = new HashSet<Object>(); //empty set
+		frontier.insert( initialNode );
+		while(true) {
+			
+			if(frontier.isEmpty())
+				return null;
+			
+			Node node = frontier.remove();
+			//get information about node
+			String cur = ( node.state + "(g=" + node.path_cost  + " , h=" + problem.h(node.state)+ ", f=" 
+			+ ((double)problem.h(node.state) + (double)node.path_cost) + ") order=" + (cnt));
+			
+			//save node info into queue
+			visited.add(cur);		
+						
+			if( problem.goal_test(node.state) ){
+				//quick function to make the order updated to reflect bucharest; so it does not return -1;
+				node.order = cnt;
+				//print the tree execution route
+				System.out.println("Entire traversal of tree in order: Execution of GraphSearch");
+				PrintTreeAllVisited(visited);
+				//print the trees solution route
+				System.out.println("Traversal of solution route: Using GraphSearch");
+				System.out.println(PrintTree(node));
+				return Solution(node);
+			}
+			
+			if( !explored.contains(node.state) ) {
+				explored.add(node.state);
+				frontier.insertAll(Expand(node,problem));
+				cnt++;
+			}
+		}
+	}
+	
+	private String TreeSearchDepthLimited(Frontier frontier, int limit) {
+		//TODO
+		return null;
+	}
+
+	private String GraphSearchDepthLimited(Frontier frontier, int limit) {
+		//TODO
+		return null;	
+	}
+
+	private Node MakeNode(Object state) {
+		Node node = new Node();
+		node.state = state;
+		node.parent_node = null;
+		node.path_cost = 0;
+		node.depth = 0;
+		return node;
+	}
+	
+	private Set<Node> Expand(Node node, Problem problem) {
+		node.order = cnt;
+		
+		Set<Node> successors = new HashSet<Node>(); //empty set
+		Set<Object> successor_states = problem.getSuccessors(node.state);
+		
+		for(Object result : successor_states) {
+			Node s = new Node();
+			s.state = result;
+			s.parent_node = node;
+			s.path_cost = node.path_cost + problem.step_cost(node.state, result); 
+			s.depth = node.depth + 1; 
+			successors.add(s);
+			
+			node_list.add( s );
+		}
+		
+		return successors;
+	}
+
+	private void PrintTreeAllVisited(Queue<String> visited){
+		
+		//error handling
+		if (visited.peek() == null){
+			System.out.println("node is null; no solution found");
+		}
+
+		for(String s : visited) { 
+  			System.out.println(s.toString()); 
+		}	
+		System.out.println();
+	}
+
+	private String PrintTree(Node node){
+
+		if (node == null){
+			String errormsg = "node is null; no solution found";
+			return errormsg;
+		}
+
+		String solution_str = "";
+		//used for printing one at a time in treesearch
+		//System.out.println( node.state + "(g=" + node.path_cost  + " , h=" + problem.h(node.state)+ ", f=" + ((double)problem.h(node.state) + (double)node.path_cost) + ") order=" + (0));
+		Deque<String> solutionname = new ArrayDeque<String>();
+		do {
+			solutionname.push(node.state.toString() + "(g= " + node.path_cost  + " , h=" + problem.h(node.state)+ ", f=" + ((double)problem.h(node.state) + (double)node.path_cost) + ") order=" + node.order);
+			node = node.parent_node;
+		} while(node != null);
+		
+		while(!solutionname.isEmpty()){
+			solution_str += solutionname.pop() + "\n";
+
+		}
+		
+		return solution_str;
+	}
+
+
+
+	//Create a string to print solution. 
+	private String Solution(Node node) {
+		
+		String solution_str = "(cost=" + node.path_cost + ", expansions=" + cnt + ")\t";
+		
+		Deque<Object> solution = new ArrayDeque<Object>();
+		do {
+			//System.out.println(node.order); for testing purposes
+			solution.push(node.state);
+			node = node.parent_node;
+		} while(node != null);
+		
+		while(!solution.isEmpty())
+			solution_str += solution.pop() + " ";
+		
+		return solution_str;
+	}
+}
